80a641f1623b24ec2b602417fff69cb9c4cec38b
Maria Riazanova <mariya.ryazanova@iqvia.com>
2024-10-21 20:11:02 +0200

    fix: fix filter function for table-tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,5 +1,13 @@
 import { simpleCalculator, Action } from './index';
 
+interface TestCase {
+  a: number | string | boolean;
+  b: number | string | boolean;
+  action: Action | string;
+  expected: number | null;
+}
+
+const invalidAction = 'invalid';
 const testCases = [
   { a: 1, b: 2, action: Action.Add, expected: 3 },
   { a: 2, b: 2, action: Action.Add, expected: 4 },
@@ -12,60 +20,28 @@ const testCases = [
   { a: 3, b: 1, action: Action.Divide, expected: 3 },
   { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
   { a: 5, b: 2, action: Action.Exponentiate, expected: 25 },
-  { a: 2, b: 3, action: 'invalid action', expected: null },
+  { a: 2, b: 3, action: invalidAction, expected: null },
   { a: '5', b: 2, action: Action.Add, expected: null },
   { a: true, b: 2, action: Action.Add, expected: null },
 ];
 
-const appropriateTestCases = (
-  action: string,
-): {
-  a: number | string | boolean;
-  b: number;
-  action: string;
-  expected: number | null;
-}[] => {
+const appropriateTestCases = (action: string): TestCase[] => {
   return filterTestCases(action);
 };
 
-const appropriateInvalidTestCases = (
-  action: string,
-): {
-  a: number | string | boolean;
-  b: number;
-  action: string;
-  expected: number | null;
-}[] => {
+const appropriateInvalidTestCases = (action: string): TestCase[] => {
   return filterTestCases(action, false);
 };
 
-const filterTestCases = (
-  action: string,
-  validCase = true,
-): {
-  a: number | string | boolean;
-  b: number;
-  action: string;
-  expected: number | null;
-}[] => {
-  return testCases.filter((testCase) =>
-    testCase.action === action && validCase
-      ? testCase.expected
-      : !testCase.expected,
+const filterTestCases = (action: string, validCase = true): TestCase[] => {
+  return testCases.filter(
+    (testCase) =>
+      testCase.action === action &&
+      (validCase ? testCase.expected !== null : testCase.expected === null),
   );
 };
 
-const testData = ({
-  a,
-  b,
-  action,
-  expected,
-}: {
-  a: number | string | boolean;
-  b: number;
-  action: string;
-  expected: number | null;
-}): void => {
+const testData = ({ a, b, action, expected }: TestCase): void => {
   const result = simpleCalculator({ a, b, action });
 
   expect(result).toBe(expected);
@@ -107,7 +83,7 @@ describe('simpleCalculator', () => {
     },
   );
 
-  test.each(appropriateTestCases('invalid action'))(
+  test.each(appropriateInvalidTestCases(invalidAction))(
     'should return null for invalid action',
     (testCase) => {
       testData(testCase);
